cf3cd808b41269d3de295d1e7ab46336ed3336ff
Kacper Zybała <zyperpl@gmail.com>
2022-08-16 01:37:24 +0200

    Add data label to WeatherEntryView

--- src/weather_entry_view.cpp
+++ src/weather_entry_view.cpp
@@ -3,22 +3,18 @@
 
 #include "imgui.h"
 
-void WeatherEntryView::set_model(std::shared_ptr<WeatherEntry> model) 
-{
+void WeatherEntryView::set_model(std::shared_ptr<WeatherEntry> model) {
   this->model = std::move(model);
 }
 
-void WeatherEntryView::render_ui() 
-{
+void WeatherEntryView::render_ui() {
   assert(model);
 
-  auto list_box_data = [](auto &container, std::string label, std::string format)
-  {
-    if (ImGui::CollapsingHeader(label.c_str()))
-    {
+  auto list_box_data = [](auto &container, std::string label,
+                          std::string format) {
+    if (ImGui::CollapsingHeader(label.c_str())) {
       ImGui::BeginListBox(label.c_str());
-      for (size_t i = 0; i < container.size(); ++i)
-      {
+      for (size_t i = 0; i < container.size(); ++i) {
         ImGui::Text(format.c_str(), container[i]);
       }
 
@@ -27,12 +23,15 @@ void WeatherEntryView::render_ui()
   };
 
   ImGui::Text("%s", model->get_name().c_str());
-  ImGui::Text("Coordinates: %f %f\nHeight: %f", model->get_latitude(), model->get_longitude(), model->get_height());
+  ImGui::Text("Coordinates: %5.4f %5.4f\nHeight: %5.2f", model->get_latitude(),
+              model->get_longitude(), model->get_height());
+  ImGui::Text("Data:\n");
   list_box_data(model->get_timestamps(), "Timestamps", "%d");
   list_box_data(model->get_lowclouds(), "Low clouds", "%4.1f%%");
   list_box_data(model->get_midclouds(), "Mid-clouds", "%4.1f%%");
   list_box_data(model->get_highclouds(), "High clouds", "%4.1f%%");
   list_box_data(model->get_cloudwater(), "Cloud water", "%5.1f g");
   list_box_data(model->get_cloudice(), "Cloud ice", "%5.1f g");
-  list_box_data(model->get_surfaceairpressure(), "Surface air pressure", "%5f hPa");
+  list_box_data(model->get_surfaceairpressure(), "Surface air pressure",
+                "%5f hPa");
 }
